d40207efb68aa202c532ca54fb037ee701c81a32
michaelgranberry <michaelgranberryii@gmail.com>
2022-12-15 18:35:28 -0800

    added scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form")
+form.addEventListener('submit', e => {
+    if (!form.checkValidity()) {
+        e.preventDefault()
+    }
+    form.classList.add('was-validated')
+})
